355860eda30b2946d12833252294e4853df3982f
gacaar <gabrielarao2013@gmail.com>
2019-07-03 13:17:48 -0300

    Escreve/Le String pronto. Inteiros incompleto

---
+++ codigosAssemblyCompletos/LeerInteiro.s
@@ -0,0 +1,79 @@
+;Funcao de leitura de inteiro
+;Argumentos: nenhum
+;Retorno: Inteiro lido na pilha[EBP+8], quantidade de caracteres lidos em AX
+section .data
+    mem db ' '
+    intLido db ' '
+    T db '1234567890'
+
+section .text
+
+;Inicio da funcao
+LeerString:
+enter 0,0
+
+;Le ate 11 caracteres (2^31 tem ateh 10 + 1 de sinal)
+mov eax, 3
+mov ebx, 0
+mov ecx, intLido
+mov edx, 11
+int 80h
+
+mov eax, 0
+cmp byte[intLido], 2Dh
+jne POSITIVO
+inc eax
+POSITIVO:
+mov edx, 0  ;inteiro
+mov ecx, 10
+mov ebx, intLido
+leitInt:
+;Encerra se for enter
+cmp byte[ebx+eax], 0Ah
+je fim
+
+;edx = 10*edx+byte[ebx+eax]
+push ecx
+shl edx, 3
+add edx, edx
+add edx, edx
+movzx ecx, byte[ebx+eax]
+shr ecx, 3*8 ;Deixa soh o byte desejado, pra nao usar registrador menor que 32 bits
+sub ecx, 30h
+add edx, ecx
+pop ecx
+
+inc eax
+loop leitInt
+fim:
+
+cmp eax, 10 ;O primeiro nao era "-"
+je POSITIVO2
+neg edx
+inc edx
+POSITIVO2:
+leave
+ret 8
+;Fim da funcao
+
+global _start
+_start: 
+
+;Inicio do trecho que deve ser traduzido, le string e coloca na posicao de memoria 'mem'
+push eax
+call LeerString
+pop eax
+mov [mem], eax
+;Fim da traducao
+
+;Exibe a string, deve ser apagado
+mov edx, eax
+mov eax, 4
+mov ebx, 1
+mov ecx, T
+int 80h
+; encerra
+
+mov eax, 1
+mov ebx, 0
+int 80h
---
+++ codigosAssemblyCompletos/LeerString.s
@@ -0,0 +1,55 @@
+;Funcao de leitura de string em ASCII
+;Argumentos: Endereco para escrever a String lida [EBP+8] e numero de caracteres para ler [EBP+12]
+;Retorno: Quantidade de caracteres lidos em EAX
+num equ 10
+section .data
+    mem db '    '
+
+section .text
+
+;Inicio da funcao
+LeerString:
+enter 0,0
+
+mov eax, 3
+mov ebx, 0
+mov ecx, [EBP+8]
+mov edx, [EBP+12]
+int 80h
+
+mov ebx, [EBP+8]
+mov ecx, [EBP+12]
+mov eax, 0
+
+leitStr:
+;Encerra se for enter
+cmp byte[ebx+eax], 0Ah
+je fim
+inc eax
+loop leitStr
+dec eax ;Nao inclui o enter se nao achar (eh incluido em seguida)
+fim:
+inc eax ;Inclui o enter se encontra-lo
+leave
+ret 8
+;Fim da funcao
+
+global _start
+_start: 
+
+;Inicio do trecho que deve ser traduzido, le string e coloca na posicao de memoria 'mem'
+push dword num  ;num eh o numero de caracteres a ser lido
+push mem        ;mem eh a label com o endereco onde a string eh salva
+call LeerString
+;Fim da traducao
+
+;Exibe a string, deve ser apagado
+mov edx, eax
+mov eax, 4
+mov ebx, 1
+mov ecx, mem
+int 80h
+;encerra
+mov eax, 1
+mov ebx, 0
+int 80h
